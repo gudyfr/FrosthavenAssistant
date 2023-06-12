7734587381ca1e0668828d7da1b37bbc0249837f
AshleySherron <49353997+AshleySherron@users.noreply.github.com>
2023-06-12 08:49:42 -0600

    fixed NAT based networking issue
    Updated the _find function to no longer return false positive matches for multiple separate connections from the same IP address.

--- frosthaven_assistant/lib/services/network/connection.dart
+++ frosthaven_assistant/lib/services/network/connection.dart
@@ -19,10 +19,10 @@ class Connection {
 
   Future<List<InternetAddress>> _resolveAddress(String address) async {
     List<InternetAddress> resolvedAddresses =
-          await InternetAddress.lookup(address);
-      if (resolvedAddresses.isEmpty) {
-        throw Exception("Unable to resolve host");
-      }
+        await InternetAddress.lookup(address);
+    if (resolvedAddresses.isEmpty) {
+      throw Exception("Unable to resolve host");
+    }
     return resolvedAddresses;
   }
 
@@ -54,7 +54,9 @@ class Connection {
   }
 
   Iterable<Socket> _find(Socket socket) {
-    return _sockets.where((x) => x.remoteAddress == socket.remoteAddress);
+    return _sockets.where((x) =>
+        x.remoteAddress == socket.remoteAddress &&
+        x.remotePort == socket.remotePort);
   }
 
   void _destroy(Iterable<Socket> sockets) {
